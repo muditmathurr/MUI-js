a3dce4068f9c00ec8767c8af3779cdc71fd58e81
muditmathurr <muditmathur05@gmail.com>
2024-07-11 18:54:31 +0530

    uncomment

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,57 @@
 import "./App.css";
 import MuiChip from "./components/MuiChip";
+import MuiList from "./components/MuiList";
+import MuiBadge from "./components/MuiBadge";
+import MuiAvatar from "./components/MuiAvatar";
+import MuiBottomNavigation from "./components/MuiBottomNavigation";
+import MuiSpeedDial from "./components/MuiSpeedDial";
+import MuiDrawer from "./components/MuiDrawer";
+import MuiNavbar from "./components/MuiNavbar";
+import MuiBreadCrumbs from "./components/MuiBreadCrumbs";
+import MuiLink from "./components/MuiLink";
+import MuiImageList from "./components/MuiImageList";
+import MuiAccordion from "./components/MuiAccordion";
+import MuiCard from "./components/MuiCard";
+import MuiLayout from "./components/MuiLayout";
+import MuiAutocomplete from "./components/MuiAutocomplete";
+import MuiRating from "./components/MuiRating";
+import MuiSwitch from "./components/MuiSwitch";
+import MuiCheckbox from "./components/MuiCheckbox";
+import MuiRadioButton from "./components/MuiRadioButton";
+import ChangeBgColor from "./components/ChangeBgColor";
+import MuiSelect from "./components/MuiSelect";
+import MuiTextField from "./components/MuiTextField";
+import MuiButton from "./components/MuiButton";
+import MuiTypography from "./components/MuiTypography";
 
-// import MuiList from "./components/MuiList";
-// import MuiBadge from "./components/MuiBadge";
-// import MuiAvatar from "./components/MuiAvatar";
-// import MuiBottomNavigation from "./components/MuiBottomNavigation";
-// import MuiSpeedDial from "./components/MuiSpeedDial";
-// import MuiDrawer from "./components/MuiDrawer";
-// import MuiNavbar from "./components/MuiNavbar";
-// import MuiBreadCrumbs from "./components/MuiBreadCrumbs";
-// import MuiLink from "./components/MuiLink";
-// import MuiImageList from "./components/MuiImageList";
-// import MuiAccordion from "./components/MuiAccordion";
-// import MuiCard from "./components/MuiCard";
-// import MuiLayout from "./components/MuiLayout";
-// import MuiAutocomplete from "./components/MuiAutocomplete";
-// import MuiRating from "./components/MuiRating";
-// import MuiSwitch from "./components/MuiSwitch";
-// import MuiCheckbox from "./components/MuiCheckbox";
-// import MuiRadioButton from "./components/MuiRadioButton";
-// import ChangeBgColor from "./components/ChangeBgColor";
-// import MuiSelect from "./components/MuiSelect";
-// import MuiTextField from "./components/MuiTextField";
-// import MuiButton from "./components/MuiButton";
-// import MuiTypography from "./components/MuiTypography";
 
 function App() {
   return (
     <>
     <MuiChip />
-      {/* <MuiList /> */}
-      {/* <MuiBadge /> */}
-      {/* <MuiAvatar /> */}
-      {/* <MuiBottomNavigation /> */}
-      {/* <MuiSpeedDial /> */}
-      {/* <MuiDrawer /> */}
-      {/* <MuiNavbar />  */}
-      {/* <MuiBreadCrumbs /> */}
-      {/* <MuiLink /> */}
-      {/* <MuiImageList /> */}
-      {/* <MuiAccordion /> */}
-      {/* <MuiCard /> */}
-      {/* <MuiLayout /> */}
-      {/* <MuiAutocomplete /> */}
-      {/* <MuiRating /> */}
-      {/* <MuiSwitch /> */}
-      {/* <MuiCheckbox /> */}
-      {/* <MuiRadioButton /> */}
-      {/* <ChangeBgColor/> */}
-      {/* <MuiSelect /> */}
-      {/* <MuiTextField /> */}
-      {/* <MuiButton /> */}
-      {/* <MuiTypography /> */}
+      <MuiList />
+      <MuiBadge />
+      <MuiAvatar />
+      <MuiBottomNavigation />
+      <MuiSpeedDial />
+      <MuiDrawer />
+      <MuiNavbar /> 
+      <MuiBreadCrumbs />
+      <MuiLink />
+      <MuiImageList />
+      <MuiAccordion />
+      <MuiCard />
+      <MuiLayout />
+      <MuiAutocomplete />
+      <MuiRating />
+      <MuiSwitch />
+      <MuiCheckbox />
+      <MuiRadioButton />
+      <ChangeBgColor/>
+      <MuiSelect />
+      <MuiTextField />
+      <MuiButton />
+      <MuiTypography />
     </>
   );
 }
